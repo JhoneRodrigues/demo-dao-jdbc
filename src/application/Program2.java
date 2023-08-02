9e50324b42ea26c389a4436b73de2b20e39340de
Jhone Rodrigues <jhonecosta30@gmail.com>
2023-08-02 12:31:04 -0300

    departmentDao delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -9,6 +10,8 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
@@ -20,10 +23,10 @@ public class Program2 {
 		List<Department>list = departmentDao.findAll();
 		list.forEach(System.out::println);
 		
-		/*System.out.println("\n=== TEST 3: department insert ====");
+		System.out.println("\n=== TEST 3: department insert ====");
 		Department newDepartment = new Department(null,"Var");
 		departmentDao.insert(newDepartment);
-		System.out.println("Insert! New id: " + newDepartment.getId());*/
+		System.out.println("Insert! New id: " + newDepartment.getId());
 		
 		System.out.println("\n=== TEST 4: department update ====");
 		dep = departmentDao.findById(5);
@@ -31,5 +34,12 @@ public class Program2 {
 		departmentDao.update(dep);
 		System.out.println("Update completed!");
 		
+		System.out.println("\n=== TEST 5: department delete ====");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 }
